23ad16c96d28406cada0cb8e303b99f147de2b39
Felix-UG <fekdonkor@st.ug.edu.gh>
2024-05-30 21:50:47 +0100

    Done with Frame 1

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+const profileImage = require("./assets/profile-image.png")
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.frame1}>
+        <View style={styles.group1}>
+          <Text style={styles.userName}>Hello , Devs</Text>
+          <Text style={styles.availableTask}>14 tasks today</Text>
+        </View>
+        <View style={styles.profile}>
+          <Image source={profileImage}/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F0E8',
+    
+    
   },
+  frame1: {
+    marginTop: 52,
+    width: wp('90%'),
+    // backgroundColor: '#ff0000',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'center'
+
+  },
+  group1: {
+    // backgroundColor: 'tomato',
+  },
+  userName: {
+    fontSize: hp('3%'),
+    fontWeight: 'bold',
+  }
 });
---
+++ README.md
@@ -0,0 +1,15 @@
+# Assignment 3 
+
+React native project Assignment 3
+
+## Task Description 
+
+In this assignment, I reacreated a design using all the components stated in the question. 
+
+## Steps 
+
+- I created the react-native folder.
+- I installed a react-native-responsive-screen to aid in the responsiveness of the app.
+- I imported the profile image from the UI design.
+- I created the Frame 1 which includes the name and the profile, and styled it to closely match the UI design given.
+- 
